b4e92aa765f440aa466f2bdd67c47e5cf8b183ea
Adam Leung <centribo@hotmail.com>
2016-11-10 19:44:35 -0500

    Started work on splash screens

---
+++ Assets/SplashScreens/ImageTransitioner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof (Image))]
+
+public class ImageTransitioner : MonoBehaviour {
+
+
+	public Image[] images;
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
