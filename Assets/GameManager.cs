b2b2cf16db3600c54a37cbbc812b77c33a4fc90a
Adam Leung <centribo@hotmail.com>
2017-05-05 14:22:41 -0400

    Added GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[AddComponentMenu("Managers/GameManager")]
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public static GameManager Instance { //Singleton pattern instance
+		get { //Getter
+			if(instance == null){ //If its null,
+				instance = (GameManager)FindObjectOfType(typeof(GameManager)); //Find it
+			}
+			return instance; //Return it
+		}
+	}
+
+	public enum State { Null };
+	public State state;
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject); //Don't destroy us on loading new scenes
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel(string level){
+		SceneManager.LoadScene(level);
+	}
+
+	public State GetState(){
+		return state;
+	}
+
+	public void SetState(State newState){
+		state = newState;
+	}
+}
