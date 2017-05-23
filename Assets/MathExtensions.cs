c0385fcb09c9485e6c5f634b42bada5d443e9f65
Adam Leung <centribo@gmail.com>
2017-05-23 05:35:57 -0400

    Added math extensions & rotate for Vector2d

---
+++ Assets/MathExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathExtensions : MonoBehaviour {
+	public static Vector2 Rotate(Vector2 v, float degrees) {
+		float radians = degrees * Mathf.Deg2Rad;
+		float sin = Mathf.Sin(radians);
+		float cos = Mathf.Cos(radians);
+
+		float tx = v.x;
+		float ty = v.y;
+
+		return new Vector2(cos * tx - sin * ty, sin * tx + cos * ty);
+	}
+}
