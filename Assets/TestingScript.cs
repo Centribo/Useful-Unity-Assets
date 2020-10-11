b8dbe5c6aecbf3665ab770d5a25d89c942e979e8
Adam Leung <adam@adamleung.me>
2020-10-10 22:08:27 -0400

    Added GetWeightedRandomElement()

--- Assets/TestingScript.cs
+++ Assets/TestingScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,12 @@ public class TestingScript : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
+		List<Tuple<int, char>> samples = new List<Tuple<int, char>> {
+			Tuple.Create(3, 'A'),
+			Tuple.Create(2, 'B'),
+			Tuple.Create(9, 'C')
+		};
+		char sample = samples.GetWeightedRandomElement();
 	}
 
 	// Update is called once per frame
