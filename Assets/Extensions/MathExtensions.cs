b1d05982f65d48f4d275a8c33a6dd61a17dbac67
Adam Leung <adam@adamleung.me>
2020-10-10 21:17:33 -0400

    Added omnisharp.json for formatting, added some new extension methods

---
+++ Assets/Extensions/MathExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathExtensions {
+
+	/// <summary>
+	/// Maps a given value in an input range to a output value in a given output range. The value will be clamped.
+	/// </summary>
+	/// <param name="value">The value in the input range [<see cref="inputMin"/>, <see cref"inputMax"/>]. This value will be clamped into the range.</param>
+	/// <param name="inputMin">The minimum value of the input range</param>
+	/// <param name="inputMax">The maximum value of the input range</param>
+	/// <param name="outputMin">The minimum value of the output range</param>
+	/// <param name="outputMax">the maximum value of the output range</param>
+	/// <returns>The linearly mapped value in the range [<see cref="outputMin"/>, <see cref="outputMax"/>]</returns>
+	public static float LinearMap(float value, float inputMin, float inputMax, float outputMin, float outputMax) {
+		float output = Mathf.Clamp(value, inputMin, inputMax);
+
+		float inputRange = inputMax - inputMin;
+		float outputRange = outputMax - outputMin;
+
+		output = ((output - inputMin) * (outputRange / inputRange)) + outputMin;
+		return output;
+	}
+}
---
+++ Assets/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public static class StringExtensions {
+	/// <summary>
+	/// Shorthand for string.Format()
+	/// </summary>
+	/// <example>
+	/// <code>
+	/// int x, y;
+	/// string coords = "({0}, {1})".Format(x, y);
+	/// </code>
+	/// </example>
+	/// <param name="s">The string to be formatted</param>
+	/// <param name="args">The arguments to be passed into string.Format()</param>
+	/// <returns>The formatted string</returns>
+	public static string Format(this string s, params object[] args) {
+		return string.Format(s, args);
+	}
+}
