773edc2a78ac7d36168e6536eee39ff6369cddbd
Adam Leung <adam@adamleung.me>
2020-10-10 20:48:05 -0400

    Updated Singleton to be from Unity3D wiki

--- Assets/Singleton.cs
+++ Assets/Singleton.cs
@@ -1,22 +1,66 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the HoloToolkit Root.
+﻿// From: http://wiki.unity3d.com/index.php/Singleton
 
-namespace UnityEngine
+using UnityEngine;
+
+/// <summary>
+/// Inherit from this base class to create a singleton.
+/// e.g. public class MyClassName : Singleton<MyClassName> {}
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-	public class Singleton<T> : MonoBehaviour where T : Singleton<T>
-	{
-		private static T _Instance;
+	// Check to see if we're about to be destroyed.
+	private static bool m_ShuttingDown = false;
+	private static object m_Lock = new object();
+	private static T m_Instance;
 
-		public static T Instance
+	/// <summary>
+	/// Access singleton instance through this propriety.
+	/// </summary>
+	public static T Instance
+	{
+		get
 		{
-			get
+			if (m_ShuttingDown)
 			{
-				if (_Instance == null)
+				Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+					"' already destroyed. Returning null.");
+				return null;
+			}
+
+			lock (m_Lock)
+			{
+				if (m_Instance == null)
 				{
-					_Instance = FindObjectOfType<T>();
+					// Search for existing instance.
+					m_Instance = (T)FindObjectOfType(typeof(T));
+
+					// Create new instance if one doesn't already exist.
+					if (m_Instance == null)
+					{
+						// Need to create a new GameObject to attach the singleton to.
+						var singletonObject = new GameObject();
+						m_Instance = singletonObject.AddComponent<T>();
+						singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+						// Make instance persistent.
+						DontDestroyOnLoad(singletonObject);
+					}
 				}
-				return _Instance;
+
+				return m_Instance;
 			}
 		}
+	}
+
+
+	private void OnApplicationQuit()
+	{
+		m_ShuttingDown = true;
+	}
+
+
+	private void OnDestroy()
+	{
+		m_ShuttingDown = true;
 	}
 }
