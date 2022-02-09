340c45d1a52b345fb0c98df78169e61567b1cec1
Adam Leung <adam@adamleung.me>
2022-02-09 09:04:15 -0500

    Updated some extensions

---
+++ Assets/Centribo-Common-Scripts/Vector3IntSerializationSurrogate.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Centribo.Common {
+	[System.Serializable]
+	/// <summary>
+	/// Basic serialization surrogate for <see cref="Vector3Int"/>
+	/// </summary>
+	public class Vector3IntSerializationSurrogate {
+		public int X, Y, Z;
+
+		public Vector3IntSerializationSurrogate(Vector3Int vector) {
+			this.X = vector.x;
+			this.Y = vector.y;
+			this.Z = vector.z;
+		}
+
+		// Cast operators:
+		public static implicit operator Vector3Int(Vector3IntSerializationSurrogate surrogate) {
+			return new Vector3Int(surrogate.X, surrogate.Y, surrogate.Z);
+		}
+
+		public static implicit operator Vector3IntSerializationSurrogate(Vector3Int original) {
+			return new Vector3IntSerializationSurrogate(original);
+		}
+	}
+}
