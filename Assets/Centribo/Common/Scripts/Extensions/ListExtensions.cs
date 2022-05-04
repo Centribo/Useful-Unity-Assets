2e85b62e5dfbef2cf8a8baf706d845aa13e76993
Adam Leung <adam@adamleung.me>
2022-05-04 17:48:48 -0400

    Moved interfaces to their own folder and added List.Resize function

--- Assets/Centribo/Common/Scripts/Extensions/ListExtensions.cs
+++ Assets/Centribo/Common/Scripts/Extensions/ListExtensions.cs
@@ -71,5 +71,28 @@ namespace Centribo.Common.Extensions {
 				return queue;
 			}
 		}
+
+		/// <summary>
+		/// Resizes a list to a specific size
+		/// </summary>
+		/// <param name="list">List to be resized</param>
+		/// <param name="newSize">The new size of the list. If invalid value is given, list is not changed</param>
+		/// <param name="defaultValue">The value to be put in the list, if the list will be expanded. Note: if using a ref type (class),
+		/// all new elements will point to the same reference!</param>
+		public static void Resize<T>(this List<T> list, int newSize, T defaultValue = default(T)) {
+			if (newSize < 0) return;
+			if (newSize == list.Count) return;
+
+			int size = list.Count;
+			if (newSize < size) {
+				list.RemoveRange(newSize, size - newSize);
+			} else if (newSize > size) {
+				if (newSize > list.Capacity) {
+					// This bit is purely an optimisation, to avoid multiple automatic capacity changes.
+					list.Capacity = newSize;
+				}
+				list.AddRange(System.Linq.Enumerable.Repeat(defaultValue, newSize - size));
+			}
+		}
 	}
 }
