b4130dcfbbf07206b2e9f284d4eada789acdafa7
Adam Leung <adam@adamleung.me>
2022-06-20 16:34:27 -0400

    Added SelectFirstObject to Panel

--- Assets/Centribo/Common/Scripts/UI/Panel.cs
+++ Assets/Centribo/Common/Scripts/UI/Panel.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Centribo.Common.UI {
 	public class Panel : MonoBehaviour {
 		[SerializeField] protected bool OpenOnStart = false;
-		
+
 		public bool IsOpen { set { if (value) { Open(); } else { Close(); } } get { return isOpen; } }
-		
+
 		protected bool isOpen;
 
 		virtual public void Start() {
@@ -21,6 +22,13 @@ namespace Centribo.Common.UI {
 			gameObject.SetActive(true);
 			isOpen = true;
 		}
+
+		/// <summary>
+		/// Sets the given event system's selected object to the "first" object that should be selected
+		/// when this panel is opened
+		/// </summary>
+		/// <param name="eventSystem"></param>
+		virtual public void SelectFirstObject(EventSystem eventSystem) { throw new System.NotImplementedException(); }
 
 		/// <summary>
 		/// Close this component, which should disable the game object and do any cleanup it needs
